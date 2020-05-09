8c1e6cfe120876bef56420a78621fe70bdf6c00f
Ping <>
2020-05-09 11:36:43 +0800

    Add basic menus

--- electron.js
+++ electron.js
@@ -1,18 +1,20 @@
-const { app, BrowserWindow } = require('electron')
+const { app, Menu, MenuItem, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   let win = new BrowserWindow({
+    title: "Mindless",
     titleBarStyle: 'hiddenInset',
     opacity: 0.98,
     width: 1050,
     height: 600,
     minHeight: 600,
     minWidth: 800,
-    // maxWidth: 1800,
+    scrollBounce: true,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      scrollBounce: true,
     }
   })
 
@@ -20,16 +22,73 @@ function createWindow () {
   win.loadFile('electron.html')
 }
 
-console.log(">>>", process.env.DEV_MODE );
+function setupMenu() {
+  const template = [
+    {
+      label: app.getName(),
+      submenu: [
+        { role: 'about' },
+        { label: 'Open directory', accelerator: 'CmdOrCtrl+O', },
+        { label: 'Reload' },
+        { type: 'separator' },
+        { role: 'services', submenu: [] },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    },
+    {
+      label: 'File',
+      submenu: [
+        { label: 'New Note', accelerator: 'CmdOrCtrl+N', },
+        { type: 'separator' },
+        { label: 'Search', accelerator: 'CmdOrCtrl+L', },
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [{ role: 'minimize' }, { role: 'zoom' }]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Github Markdown Syntax',
+          click() {
+            require('electron').shell.openExternal('https://help.github.com/en/github/writing-on-github/basic-writing-and-formatting-syntax');
+          }
+        }
+      ]
+    }
+  ];
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
+
+console.log(">>>", process.env.DEV_MODE);
 
 const isDev = process.env.DEV_MODE ? (process.env.DEV_MODE.trim() == "true") : false;
 
 if (isDev) {
-    require('electron-reload')(__dirname, {
-        electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
-        hardResetMethod: 'exit'
-      });
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+    hardResetMethod: 'exit'
+  });
 }
 
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow).then(setupMenu);
